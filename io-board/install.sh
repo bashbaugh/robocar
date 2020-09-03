a086310304ee5631663fd619c3852f96181e072b
Benjamin Ashbaugh <scitronboy@gmail.com>
2020-09-02 21:29:49 -0600

    Delete install.sh

--- io-board/install.sh
+++
@@ -1,9 +0,0 @@
-
-# Install arduino and avrdude for programming the ATmega through SPI from the raspberry pi
-# SPI must be enabled through raspi-config
-
-sudo apt update
-
-sudo apt install arduino arduino-mk avrdude -y
-
-sudo avrdude -p atmega328p -c linuxspi -P /dev/spidev0.0
